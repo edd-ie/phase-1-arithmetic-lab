f476bb2a4e8927dd8c1c9a00c4d30d192bd31492
edd-ie <ogendoeddie@gmail.com>
2023-03-13 14:33:02 +0300

    Instruction unclear but complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+// TODO: multiply, equation = 62
+let num1 = 31;
+let num2 = 2;
+const multiply = (num1 * num2)
+
+
+//TODO: random, generates integer greater than 0
+const random = Math.ceil(Math.random())
+
+
+
+//TODO: mod, an equation that calculates a remainder that is equal to 4
+let num3 = 24;
+let num4 = 5;
+const mod = (num3%num4);
+
+// TODO: max, return 20 as the highest number in the set
+const max = Math.max(5,10,15,20);
